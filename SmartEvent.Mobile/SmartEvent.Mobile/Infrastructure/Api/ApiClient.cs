7c9ef2ab9502f17256ed1eebe3c2f8d2562fb9fc
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-07 21:19:17 +0300

    feat: add api client #17

--- SmartEvent.Mobile/SmartEvent.Mobile/Infrastructure/Api/ApiClient.cs
+++ SmartEvent.Mobile/SmartEvent.Mobile/Infrastructure/Api/ApiClient.cs
@@ -1,68 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
+﻿using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
+using SmartEvent.Mobile.Core.Common;
 
 
-namespace SmartEvent.Mobile.Infrastructure.Api
+namespace SmartEvent.Mobile.Infrastructure.Api;
+
+public class ApiClient(HttpClient httpClient) : IApiClient
 {
-    public class ApiClient : IApiClient
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new() {PropertyNameCaseInsensitive =  true};
+
+    public Task<ApiResult<TResponse>> GetAsync<TResponse>(string url) =>
+        ExecuteAsync<TResponse>(() =>
+            httpClient.GetAsync(url));
+
+    public Task<ApiResult<TResponse>> PostAsync<TRequest, TResponse>(string url, TRequest payload) =>
+        ExecuteAsync<TResponse>(() 
+            =>  httpClient.PostAsJsonAsync(url, payload));
+
+    public Task<ApiResult<TResponse>> PutAsync<TRequest, TResponse>(string url, TRequest payload) =>
+        ExecuteAsync<TResponse>(() =>
+            httpClient.PutAsJsonAsync(url, payload));
+
+    public Task<ApiResult<TResponse>> DeleteAsync<TResponse>(string url) =>
+        ExecuteAsync<TResponse>(() =>
+            httpClient.DeleteAsync(url));
+
+    private async Task<ApiResult<TResponse>> ExecuteAsync<TResponse>(Func<Task<HttpResponseMessage>> requestAction)
     {
-        private readonly HttpClient _http;
-        public ApiClient(HttpClient http)
+        try
         {
-            _http = http;
-        }
+            using var response = await requestAction();
 
-        public async Task<ApiResult<T>> GetAsync<T>(string url)
-        {
-            var response = await _http.GetAsync(url);
-            return await HandleResponse<T>(response);
-        }
-
-        public async Task<ApiResult<T>> PostAsync<T>(string url, object body)
-        {
-            var json = JsonSerializer.Serialize(body);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var response = await _http.PostAsync(url, content);
-            return await HandleResponse<T>(response);
-        }
-
-        public async Task<ApiResult<T>> PutAsync<T>(string url, object body)
-        {
-            var json = JsonSerializer.Serialize(body);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var response = await _http.PutAsync(url, content);
-            return await HandleResponse<T>(response);
-        }
-        public async Task<ApiResult> DeleteAsync(string url)
-        {
-            var response = await _http.DeleteAsync(url);
-            return await HandleResponse<object>(response);
-        }
-
-        private async Task<ApiResult<T>> HandleResponse<T>(HttpResponseMessage response)
-        {
-            var result = new ApiResult<T>
-            {
-                StatusCode = (int)response.StatusCode,
-                IsSuccess = response.IsSuccessStatusCode
-            };
-            if (response.StatusCode == HttpStatusCode.NoContent)
-                return result;
-            var json = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
-                result.Data = JsonSerializer.Deserialize<T>(json);
-            }
-            else
-            {
-                var problem = JsonSerializer.Deserialize<ProblemDetails>(json);
-                result.Error = problem?.Detail ?? "Unknown error";
-            }
-            return result;
-        }
+                if (response.StatusCode == HttpStatusCode.NoContent)
+                    return ApiResult<TResponse>.Success(default!, response.StatusCode);
 
+                var content = await response.Content.ReadFromJsonAsync<TResponse>(_jsonSerializerOptions);
+                return ApiResult<TResponse>.Success(content!, response.StatusCode);
+            }
+                
+            var parsedResponseWithFail = await ParseProblemDetailsAsync<TResponse>(response);
+            return parsedResponseWithFail;
+        }
+        catch (HttpRequestException)
+        {
+            return ApiResult<TResponse>.Failure("Network error", (int)HttpStatusCode.BadRequest, null);
+        }
+        catch (TaskCanceledException)
+        {
+            return ApiResult<TResponse>.Failure("Network error", (int)HttpStatusCode.RequestTimeout, null);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            return ApiResult<TResponse>.Failure("Server is sleeping", (int)HttpStatusCode.InternalServerError, null);
+        }
+    }
+
+    private async Task<ApiResult<T>> ParseProblemDetailsAsync<T>(HttpResponseMessage response)
+    {
+        try
+        {
+            var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>(_jsonSerializerOptions);
+
+            if (problemDetails != null)
+            {
+                var errorMessage = problemDetails.Detail;
+                var errorCode = problemDetails.Status;
+
+                return ApiResult<T>.Failure(errorMessage, errorCode, null);
+            }
+        }
+        catch (JsonException)
+        {
+            return ApiResult<T>.Failure("Bad request format!", (int)HttpStatusCode.InternalServerError, null);
+        }
+            
+        return ApiResult<T>.Failure("Unknown server error! Try later)", (int)HttpStatusCode.InternalServerError, null);
     }
 }
