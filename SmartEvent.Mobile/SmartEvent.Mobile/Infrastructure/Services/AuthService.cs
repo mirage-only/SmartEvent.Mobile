b68a232dea86cc8790d832337af0e76bf0ff86d9
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-07 21:21:42 +0300

    feat: realize authService #17

--- SmartEvent.Mobile/SmartEvent.Mobile/Infrastructure/Services/AuthService.cs
+++ SmartEvent.Mobile/SmartEvent.Mobile/Infrastructure/Services/AuthService.cs
@@ -1,41 +1,51 @@
-﻿using SmartEvent.Mobile.Core.DTOs.UserDTOs.Requests;
+﻿using SmartEvent.Mobile.Core.Common;
+using SmartEvent.Mobile.Core.DTOs.UserDTOs.Requests;
 using SmartEvent.Mobile.Core.DTOs.UserDTOs.Responses;
-using SmartEvent.Mobile.Core.IServices;
+using SmartEvent.Mobile.Core.Interfaces.IServices;
 using SmartEvent.Mobile.Infrastructure.Api;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SmartEvent.Mobile.Infrastructure.Services
 {
-    public class AuthService : IAuthService
+    public class AuthService: IAuthService
     {
         private readonly IApiClient _apiClient;
+
         public AuthService(IApiClient apiClient)
         {
             _apiClient = apiClient;
         }
-
-        public async Task<string?> Login(LoginUserRequestDto dto)
+        public async Task<ApiResult<AuthorizeUserResponseDto>> RegisterAsync(RegisterUserRequestDto dto)
         {
-            //var result = await _apiClient.PostAsync<AuthorizeUserResponseDto>("/login", dto);
-            //if (!result.IsSuccess)
-            //    throw new Exception(result.Error);
-            //if (result.Data == null) 
-            //    throw new Exception("Empty response from server");
-            //return result.Data.JwtToken;
-            return "jwt login here!";
+            string url = ApiRoutes.RegisterUrl;
+            
+            var result = await _apiClient.PostAsync<RegisterUserRequestDto, AuthorizeUserResponseDto>(url, dto);
+
+            if (result.IsSuccess && result.Data != null)
+            {
+                await SecureStorage.Default.SetAsync("jwt_token", result.Data.JwtToken);
+            }
+            
+            return result;
         }
 
-        public async Task<string?> Register(RegisterUserRequestDto dto)
+        public async Task<ApiResult<AuthorizeUserResponseDto>> LoginAsync(LoginUserRequestDto dto)
         {
-            //var result = await _apiClient.PostAsync<AuthorizeUserResponseDto>("/register", dto);
-            //if (!result.IsSuccess)
-            //    throw new Exception(result.Error);
-            //if (result.Data == null) 
-            //    throw new Exception("Empty response from server");
-            //return result.Data.JwtToken;
-            return "jwt reg here!";
+            string url = ApiRoutes.LoginUrl;
+            
+            var result = await _apiClient.PostAsync<LoginUserRequestDto, AuthorizeUserResponseDto>(url, dto);
+
+            if (result.IsSuccess && result.Data != null)
+            {
+                //TODO: create service for work with storage
+                await SecureStorage.Default.SetAsync("jwt_token", result.Data.JwtToken);
+            }
+            
+            return result;
+        }
+
+        public Task<ApiResult<EmptyResponse>> LogoutAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
