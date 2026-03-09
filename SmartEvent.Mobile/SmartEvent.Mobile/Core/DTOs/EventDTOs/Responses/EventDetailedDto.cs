4084172b0dc1fe3a787b2faceb834bb9b4999f69
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-09 22:09:32 +0300

    feat (dto): add eventDetailedDto #3

---
+++ SmartEvent.Mobile/SmartEvent.Mobile/Core/DTOs/EventDTOs/Responses/EventDetailedDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartEvent.Mobile.Core.DTOs.EventDTOs.Responses
+{
+    public class EventDetailedDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public DateTime StartTime { get; set; } = DateTime.UtcNow;
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
