5124cad925853d9cee856d71fb6087a6ed3ddfe0
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-10 23:07:20 +0300

    feat: add route in appShell #3

--- SmartEvent.Mobile/SmartEvent.Mobile/AppShell.xaml.cs
+++ SmartEvent.Mobile/SmartEvent.Mobile/AppShell.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace SmartEvent.Mobile;
+using SmartEvent.Mobile.Core.DTOs.EventDTOs.Responses;
+using SmartEvent.Mobile.Presentation.Views;
+
+namespace SmartEvent.Mobile;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        
+        Routing.RegisterRoute(nameof(EventDetailsPage), typeof(EventDetailsPage));
     }
 }
