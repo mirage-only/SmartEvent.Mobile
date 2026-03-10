9c5b12454220d8fca0f2be00fe68c4c0b6368a9f
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-10 23:06:16 +0300

    feat: edit route for event details #3

--- SmartEvent.Mobile/SmartEvent.Mobile/Infrastructure/Services/ApiRoutes.cs
+++ SmartEvent.Mobile/SmartEvent.Mobile/Infrastructure/Services/ApiRoutes.cs
@@ -2,12 +2,14 @@
 
 public static class ApiRoutes
 {
-    public const string BaseUrl = "http://192.168.1.124:5187";
+    public const string BaseUrl = "http://192.168.1.12:5187";
     
     private const string UserUrl = $"{BaseUrl}/users";
     
     private const string EventUrl = $"{BaseUrl}/events";
     public static readonly string GetLightEvents = $"{EventUrl}/getLightEventsWithPagination";
+    public static readonly string GetEventDetails = $"{EventUrl}/get";
+    
     
     private const string AuthUrl = $"{BaseUrl}/users/auth";
     public static readonly string RegisterUrl = $"{AuthUrl}/register";
