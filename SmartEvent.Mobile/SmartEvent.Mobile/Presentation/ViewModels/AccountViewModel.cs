17869b7f6f09b74b21d568e22ecd464f30882d90
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-12 18:49:07 +0300

    refactor: ref style #3

--- SmartEvent.Mobile/SmartEvent.Mobile/Presentation/ViewModels/AccountViewModel.cs
+++ SmartEvent.Mobile/SmartEvent.Mobile/Presentation/ViewModels/AccountViewModel.cs
@@ -1,25 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SmartEvent.Mobile.Core.DTOs.EventDTOs.Responses;
+using SmartEvent.Mobile.Core.Interfaces.IServices;
 
 namespace SmartEvent.Mobile.Presentation.ViewModels;
 
 public partial class AccountViewModel : ObservableObject
 {
-    [ObservableProperty]
-    private string? firstName;
+    
+    [ObservableProperty] private string? firstName;
 
-    [ObservableProperty]
-    private string? email;
+    [ObservableProperty] private string? email;
 
-    [ObservableProperty] 
-    private List<EventLightDto> registeredEvents; 
+    [ObservableProperty] private List<EventLightDto> registeredEvents; 
 
-    [ObservableProperty] 
-    private List<EventLightDto> visitedEvents; 
+    [ObservableProperty] private List<EventLightDto> visitedEvents; 
 
-    [ObservableProperty] 
-    private List<EventLightDto> currentEvents;
+    [ObservableProperty] private List<EventLightDto> currentEvents;
 
 
 
