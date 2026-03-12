11c931049631ce404b104e0d4378673061b438b3
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-12 18:46:50 +0300

    feat: add userContext #3

---
+++ SmartEvent.Mobile/SmartEvent.Mobile/Core/Interfaces/IServices/IUserContext.cs
@@ -0,0 +1,13 @@
+﻿using SmartEvent.Mobile.Core.Common;
+
+namespace SmartEvent.Mobile.Core.Interfaces.IServices;
+
+public interface IUserContext
+{
+    Guid UserId { get; }
+    string UserEmail { get; }
+    UserRole UserRole { get; }
+    bool IsAuthenticated { get; }
+    void SetUser(string token);
+    void Clear();
+}
---
+++ SmartEvent.Mobile/SmartEvent.Mobile/Infrastructure/Services/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SmartEvent.Mobile.Core.Common;
+using SmartEvent.Mobile.Core.Interfaces.IServices;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace SmartEvent.Mobile.Infrastructure.Services;
+
+public partial class UserContext: ObservableObject, IUserContext
+{
+    [ObservableProperty] private Guid _userId =  Guid.Empty;
+    [ObservableProperty] private string _userEmail =  string.Empty;
+    [ObservableProperty] private UserRole _userRole =   UserRole.Guest;
+    
+    public bool IsAuthenticated => UserId != Guid.Empty;
+    
+    public void SetUser(string token)
+    }
