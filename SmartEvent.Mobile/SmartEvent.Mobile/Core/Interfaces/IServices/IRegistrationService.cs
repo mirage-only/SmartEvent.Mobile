6f33f76ff3e1e621a9eb2aebc13b518e91bf2088
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-12 18:50:51 +0300

    feat (service):  add RegistrationService #3

---
+++ SmartEvent.Mobile/SmartEvent.Mobile/Core/Interfaces/IServices/IRegistrationService.cs
@@ -0,0 +1,8 @@
+﻿using SmartEvent.Mobile.Core.Common;
+
+namespace SmartEvent.Mobile.Core.Interfaces.IServices;
+
+public interface IRegistrationService
+{
+    public Task<ApiResult<Guid>> RegisterForEvent(Guid eventId);
+}
