7a9db92d6ea68747e8f94a78604c5b6b1a60f5b2
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-16 23:31:15 +0300

    feat (EventDetailsDto): add LocDto&Room #27

--- SmartEvent.Mobile/SmartEvent.Mobile/Core/DTOs/EventDTOs/Responses/EventDetailsDto.cs
+++ SmartEvent.Mobile/SmartEvent.Mobile/Core/DTOs/EventDTOs/Responses/EventDetailsDto.cs
@@ -7,8 +7,8 @@
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
         public DateTime StartTime { get; set; } = DateTime.UtcNow;
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
+        public LocationDto Location { get; set; } = new LocationDto();
+        public string Room { get; set; } = string.Empty;
         public Guid CreatorId { get; set; }
     }
 }
